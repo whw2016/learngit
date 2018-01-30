4b7684fcb93c3c98d7b2f56fd1f095ffd01e78a5
Hongwei Wang <516322644@qq.com>
2018-01-30 11:29:27 +0800

    test.php

---
+++ temp/test.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: wanghongwei
+ * Date: 2018/1/30
+ * Time: 11:23
+ */
+
+$arr = [2=>"w","a","n","g"];
+var_dump($arr);
+
+//负数下一个是从0开始的。
+$arr = [-2=>"w","a","n","g"];
+var_dump($arr);
